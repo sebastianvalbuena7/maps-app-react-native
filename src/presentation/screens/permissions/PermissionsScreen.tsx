4ed78e2c2272f8c23fc9a89ef802525645e0fefc
Sebastian <sebastianvalbuena25@gmail.com>
2024-04-20 12:41:22 -0500

    Creando proyecto

---
+++ src/presentation/screens/permissions/PermissionsScreen.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native"
+
+export const PermissionsScreen = () => {
+    return (
+        <View>
+            <Text>PermissionsScreen</Text>
+        </View>
+    )
+}
